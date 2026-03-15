ee277a5401d9188fc74c80102f7b7129da355311
Yashwanth Sai <yashwanthkesagani008@gmail.com>
2026-03-16 05:14:15 +0530

    Create SquareWallPattern.java

---
+++ SquareWallPattern.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static void squareWall(int s) {
+        for(int i=1;i<=s;i++)
+        {
+            for(int j=1;j<=s;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
