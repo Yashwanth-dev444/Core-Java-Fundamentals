a182a8ebadc16a40e1813649d84b75e225d4f841
Yashwanth Sai <yashwanthkesagani008@gmail.com>
2026-03-24 09:07:58 +0530

    Create AreStringsEquals.java

---
+++ AreStringsEquals.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static boolean areEqual(String s1, String s2) {
+        boolean x;
+        if(s1.equals(s2))
+        {
+            x=true;
+        }
+        else
+        {
+            x=false;
+        }
+        return x;
+    }
+}
