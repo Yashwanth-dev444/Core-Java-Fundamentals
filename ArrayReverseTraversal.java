344b7741c15448550e51f4ba1c4f04de8c9a8d4a
Yashwanth Sai <yashwanthkesagani008@gmail.com>
2026-03-28 11:31:59 +0530

    Create ArrayReverseTraversal.java

---
+++ ArrayReverseTraversal.java
@@ -0,0 +1,8 @@
+class ArrayReverseTraversal {
+    public static void arrayTraversalReverse(int[] arr) {
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
