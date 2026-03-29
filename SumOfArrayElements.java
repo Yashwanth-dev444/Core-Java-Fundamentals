7f18f9fe2476e569fa70b1a6e7efe2fefbb46272
Yashwanth Sai <yashwanthkesagani008@gmail.com>
2026-03-29 07:29:16 +0530

    Create SumOfArrayElements.java

---
+++ SumOfArrayElements.java
@@ -0,0 +1,10 @@
+class SumOfArrayElements{
+    public static int arraySum(int[] arr) {
+        int sum=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            sum=sum+arr[i];
+        }
+        return sum;
+    }
+}
