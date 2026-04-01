74136b4248dc7273db22a091a9b8cb476dd28dac
Yashwanth Sai <yashwanthkesagani008@gmail.com>
2026-04-01 09:25:08 +0530

    Create FunctionWithArguments.java

---
+++ FunctionWithArguments.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static void main (String[] args)
+    {
+        int a=2,b=3;
+        int x=argumentFunction(a,b);
+        System.out.println(x);
+    }
+    public static int argumentFunction(int a,int b)
+    {
+        return (a+b);
+    }
+}
