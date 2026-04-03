55780365e7bb9c5cc9d18602b70ec6ffbfd4d104
Yashwanth Sai <yashwanthkesagani008@gmail.com>
2026-04-03 20:12:33 +0530

    Create HalistoneSequence.java

---
+++ HalistoneSequence.java
@@ -0,0 +1,17 @@
+public class HalistoneSequence
+{
+	public static void main(String[] args) {
+		int n=6;
+		while(n!=1).      
+		{
+		      if(n%2==0){
+		            n/=2;
+		            System.out.print(n+" ");
+		      }
+		      else{
+		            n=(n*3)+1;
+		            System.out.print(n+" ");
+		      }
+		}
+	}
+}
